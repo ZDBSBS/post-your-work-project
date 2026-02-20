3ed109e12e5cdf2f36454ef77795f893136c9264
Zied Besbes <GUGCRDU@vw.vwgroup.com>
2026-02-20 15:15:24 +0100

    refactor: add spacing around operator in separator print for readability

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -24,7 +24,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
 
-    print('-'*40)
+    print('-' * 40)
     return city, month, day
 
 
@@ -60,7 +60,7 @@ def time_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
 
 
 def station_stats(df):
@@ -79,7 +79,7 @@ def station_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
 
 
 def trip_duration_stats(df):
@@ -95,7 +95,7 @@ def trip_duration_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
 
 
 def user_stats(df):
@@ -114,7 +114,7 @@ def user_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
 
 
 def main():
